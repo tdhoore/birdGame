1db5427b722a9970100ea852841e3fd2b04b03c2
tdhoore <timdhoore@gmail.com>
2018-12-21 16:36:10 +0100

    movement

---
+++ birdgame/Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 0f;
+
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start() {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update() {
+        //calc movemenent
+        Vector3 moveVertical = transform.forward;
+
+        //final movement
+        Vector3 velocity = transform.forward.normalized * speed;
+
+        //apply movement
+        motor.Move(velocity);
+
+        //rotation
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _rotation = new Vector3(_xRot, _yRot, 0) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+    }
+}
---
+++ birdgame/Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void Move(Vector3 inputV) {
+        velocity = inputV;
+    }
+
+    public void Rotate(Vector3 _rotation) {
+        rotation = _rotation;
+    }
+
+    private void FixedUpdate() {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement() {
+        if (velocity != Vector3.zero) {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation() {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+    }
+}
